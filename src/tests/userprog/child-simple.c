5d47ab697c5410968128c5968033f95d964a4b6e
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-05-25 04:01:54 -0700

    clang-format: bulk reformat entire codebase

--- src/tests/userprog/child-simple.c
+++ src/tests/userprog/child-simple.c
@@ -2,14 +2,15 @@
    wait-twice tests.
    Just prints a single message and terminates. */
 
-#include <stdio.h>
 #include "tests/lib.h"
 
-int
-main (void) 
-{
-  test_name = "child-simple";
+#include <stdio.h>
 
-  msg ("run");
-  return 81;
+int
+main(void)
+{
+	test_name = "child-simple";
+
+	msg("run");
+	return 81;
 }
--- src/tests/userprog/open-null.c
+++ src/tests/userprog/open-null.c
@@ -1,12 +1,13 @@
 /* Tries to open a file with the null pointer as its name.
    The process must be terminated with exit code -1. */
 
-#include <stddef.h>
-#include <syscall.h>
 #include "tests/main.h"
 
+#include <stddef.h>
+#include <syscall.h>
+
 void
-test_main (void) 
+test_main(void)
 {
-  open (NULL);
+	open(NULL);
 }
