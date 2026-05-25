b15b9ae095e37c6f8e5e69d709450d01f3ac7469
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-05-25 04:01:54 -0700

    clang-format: bulk reformat entire codebase

--- src/tests/userprog/open-missing.c
+++ src/tests/userprog/open-missing.c
@@ -1,13 +1,14 @@
 /* Tries to open a nonexistent file. */
 
-#include <syscall.h>
 #include "tests/lib.h"
 #include "tests/main.h"
 
+#include <syscall.h>
+
 void
-test_main (void) 
+test_main(void)
 {
-  int handle = open ("no-such-file");
-  if (handle != -1)
-    fail ("open() returned %d", handle);
+	int handle = open("no-such-file");
+	if (handle != -1)
+		fail("open() returned %d", handle);
 }
