7673282e0b3f3a7ddda3d7eaac521ba25647cf62
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-05-29 04:42:17 -0700

    checkpoint minimal write() syscall stub; works for printf("\n"), but hard to tell beyond that because argument-passing seems buggy

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -2,6 +2,7 @@
 
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "userprog/pagedir.h"
 
 #include <stdio.h>
 #include <syscall-nr.h>
@@ -17,6 +18,40 @@ syscall_init(void)
 static void
 syscall_handler(struct intr_frame *f UNUSED)
 {
+	uint32_t *pagedir = thread_current()->pagedir;
+
+	// TODO: validate esp before calling pagedir_get_page
+	long *upage = pagedir_get_page(pagedir, f->esp);
+	ASSERT(upage != NULL); // TODO: error cleanly
+	printf("got upage %p\n", upage);
+
+	const long syscall_number = *upage;
+	printf("got syscall %ld\n", syscall_number);
+	ASSERT(syscall_number == SYS_WRITE); // TODO validate syscall_number
+
+	++upage;
+
+	const long fd = *upage;
+	printf("got fd %ld\n", fd);
+	ASSERT(fd == STDOUT_FILENO); // TODO validate fd
+
+	++upage;
+
+	const uintptr_t buffer_uaddr = *upage;
+	printf("got buffer_uaddr %p\n", (void *)buffer_uaddr);
+
+	// TODO: validate buffer_uaddr before calling pagedir_get_page
+	void *buffer_paddr = pagedir_get_page(pagedir, (void *)buffer_uaddr);
+	printf("got buffer_paddr %p\n", (void *)buffer_paddr);
+
+	++upage;
+
+	const long sz = *upage; // TODO: clamp buffer size?
+	printf("got size %ld\n", sz);
+
+	printf("best-effort buffer_paddr data:\n");
+	hex_dump(buffer_uaddr, buffer_paddr, sz, true); // TODO rm hex_dump()
+
 	printf("system call!\n");
 	thread_exit();
 }
