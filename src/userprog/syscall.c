635360a7bfdc1d56d7e69231d34c35e8e6f6ac36
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-05-30 14:02:05 -0700

    add hacky fix for sc-bad-arg plus TODO to generalize

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -279,6 +279,11 @@ syscall_handler(struct intr_frame *f)
 		thread_exit_invalid_pointer_argument(f);
 	}
 
+	if (!is_user_vaddr(uaddr + sizeof(int))) {
+		// TODO: generalize fix for sc-bad-arg
+		thread_exit_invalid_pointer_argument(f);
+	}
+
 	if (pg_ofs(uaddr) + 1 >= PGSIZE) {
 		// TODO: generalize fix for sc-boundary-3 to
 		// syscall_arg_peek(), other stack access like syscall_read()
