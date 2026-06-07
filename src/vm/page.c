e9df24a3c7f904e2f6692b62780a7d7f2eef5a20
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-06-07 00:47:13 -0700

    use file_write_at/file_read_at, avoid using file_seek unnecessarily in page.c fuinctions

--- src/vm/page.c
+++ src/vm/page.c
@@ -76,11 +76,8 @@ page_evict_prepare(struct thread *t,
 		struct file *file = fd_to_file(entry->file.fd);
 		ASSERT(file != NULL);
 		acquire_io_lock();
-		// TODO: switch file_seek+file_write -> file_write_at
-		file_seek(file, entry->file.pos);
-		const off_t eof = file_length(file);
-		const off_t bytes = file_write(file, kpage, PGSIZE);
-		ASSERT(bytes == PGSIZE || bytes + entry->file.pos == eof);
+		off_t b = file_write_at(file, kpage, PGSIZE, entry->file.pos);
+		ASSERT(b == PGSIZE || b + entry->file.pos == file_length(file));
 		release_io_lock();
 	}
 
@@ -272,11 +269,7 @@ page_fault_impl(struct intr_frame *f, void *uaddr, void **kpage_out)
 		ASSERT(file != NULL);
 		ASSERT(intr_get_level() == INTR_ON);
 		acquire_io_lock();
-		const off_t to_restore = file_tell(file);
-		// TODO: switch file_seek+file_read -> file_read_at
-		file_seek(file, pos);
-		const off_t bytes = file_read(file, kpage, PGSIZE);
-		file_seek(file, to_restore);
+		const off_t bytes = file_read_at(file, kpage, PGSIZE, pos);
 		release_io_lock();
 		if (bytes < PGSIZE) {
 			memset(kpage + bytes, 0, PGSIZE - bytes);
