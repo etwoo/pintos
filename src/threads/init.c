038cdf1cbadbff31fb0b043a9a18c573198c1b63
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-06-09 19:42:58 -0700

    pick a smaller writeback period, most tests probably do not last 60s, use a smaller interval to improve the chances of having some coverage

--- src/threads/init.c
+++ src/threads/init.c
@@ -133,7 +133,7 @@ main(void)
 	/* Initialize file system. */
 	ide_init();
 	locate_block_devices();
-	filesys_init(format_filesys, 60 * 1000 /* 60s */);
+	filesys_init(format_filesys, 5 * 1000 /* 5s */);
 #endif
 #ifdef VM
 	swap_init();
