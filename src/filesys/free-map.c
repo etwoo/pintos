8a5247972b2b45c0d7cafda59a1aef71cf3d2d61
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-06-13 13:16:44 -0700

    resolve TODO re: copy-pasta in free-map.c; consolidate into common serializable_bitmap struct + helper functions

--- src/filesys/free-map.c
+++ src/filesys/free-map.c
@@ -9,83 +9,64 @@
 #include <debug.h>
 #include <round.h>
 
-// TODO: wrap these fields up into a struct, then reduce copy-pasta
-static void *inode_map_memory = NULL;
-static struct bitmap *inode_map = NULL; /* Free map, one bit per inode. */
-static block_sector_t inode_map_sector = UINT32_MAX;
-static block_sector_t inode_map_sector_count = UINT32_MAX;
+struct serializable_bitmap {
+	// TODO: lock? or concurrency handled at higher level?
+	void *memory;
+	struct bitmap *bitmap;
+	block_sector_t sector;
+	block_sector_t sector_count;
+};
 
-static void *free_map_memory = NULL;
-static struct bitmap *free_map = NULL; /* Free map, one bit per sector. */
-static block_sector_t free_map_sector = UINT32_MAX;
-static block_sector_t free_map_sector_count = UINT32_MAX;
+static struct serializable_bitmap inode_map;
+static struct serializable_bitmap block_map;
+
+static void
+map_alloc(struct serializable_bitmap *sb, size_t bitcount)
+{
+	ASSERT(sb->sector != 0);
+
+	const size_t buffer_size = bitmap_buf_size(bitcount);
+	sb->sector_count = DIV_ROUND_UP(buffer_size, BLOCK_SECTOR_SIZE);
+
+	sb->memory = malloc(buffer_size);
+	if (sb->memory == NULL) {
+		PANIC("bitmap creation failed -- too many inodes or blocks");
+	}
+
+	sb->bitmap = bitmap_create_in_buf(bitcount, sb->memory, buffer_size);
+	ASSERT(sb->bitmap != NULL);
+}
 
 /* Initializes the free map. */
 void
 free_map_init(block_sector_t start_sector)
 {
-	inode_map_sector = start_sector;
-	inode_map_sector_count =
-		DIV_ROUND_UP(bitmap_buf_size(INODE_LIMIT), BLOCK_SECTOR_SIZE);
+	inode_map.sector = start_sector;
+	map_alloc(&inode_map, INODE_LIMIT);
 
-	inode_map_memory = malloc(bitmap_buf_size(INODE_LIMIT));
-	if (inode_map_memory == NULL) {
-		PANIC("bitmap creation failed -- too many inodes");
-	}
-	inode_map = bitmap_create_in_buf(INODE_LIMIT,
-	                                 inode_map_memory,
-	                                 bitmap_buf_size(INODE_LIMIT));
-	ASSERT(inode_map != NULL);
+	block_map.sector = inode_map.sector + inode_map.sector_count;
+	map_alloc(&block_map, block_size(fs_device));
 
-	const block_sector_t fs_sector_count = block_size(fs_device);
-
-	free_map_sector = inode_map_sector + inode_map_sector_count;
-	free_map_sector_count = DIV_ROUND_UP(bitmap_buf_size(fs_sector_count),
-	                                     BLOCK_SECTOR_SIZE);
-
-	free_map_memory = malloc(bitmap_buf_size(fs_sector_count));
-	if (free_map_memory == NULL) {
-		PANIC("bitmap creation failed -- block device is too large");
-	}
-	free_map = bitmap_create_in_buf(fs_sector_count,
-	                                free_map_memory,
-	                                bitmap_buf_size(fs_sector_count));
-	ASSERT(free_map != NULL);
-
-	bitmap_set_multiple(free_map,
+	bitmap_set_multiple(block_map.bitmap,
 	                    INOFILE_SECTOR,
-	                    free_map_sector + free_map_sector_count,
+	                    block_map.sector + block_map.sector_count,
 	                    true);
 }
 
-static const uint32_t WRITE_INODE_MAP = 0x1;
-static const uint32_t WRITE_FREE_MAP = 0x2;
-
 static bool
-free_map_write(uint32_t flags)
+map_write(struct serializable_bitmap *sb)
 {
-	ASSERT(inode_map_sector != UINT32_MAX);
-	ASSERT(inode_map_sector_count != UINT32_MAX);
-	ASSERT(free_map_sector != UINT32_MAX);
-	ASSERT(free_map_sector_count != UINT32_MAX);
-	const bool imap = ((flags & WRITE_INODE_MAP) != 0);
-	const bool fmap = ((flags & WRITE_FREE_MAP) != 0);
-	const int sz = BLOCK_SECTOR_SIZE;
+	ASSERT(sb->sector != 0);
+	ASSERT(sb->sector_count != 0);
 	bool ok = true;
 
 	/* Blit structure to disk. Would be better to deal with endianness and
 	 * generally make serialization more portable. Live with it for now. */
-
-	for (block_sector_t i = 0; imap && i < inode_map_sector_count; ++i) {
-		const void *buf =
-			((void *)inode_map_memory) + (i * BLOCK_SECTOR_SIZE);
-		ok = cache_write(inode_map_sector + i, 0, sz, buf) && ok;
-	}
-
-	for (block_sector_t i = 0; fmap && i < free_map_sector_count; ++i) {
-		const void *buf =
-			((void *)free_map_memory) + (i * BLOCK_SECTOR_SIZE);
-		ok = cache_write(free_map_sector + i, 0, sz, buf) && ok;
+	for (block_sector_t i = 0; i < sb->sector_count; ++i) {
+		const void *buf = sb->memory + (i * BLOCK_SECTOR_SIZE);
+		if (!cache_write(sb->sector + i, 0, BLOCK_SECTOR_SIZE, buf)) {
+			ok = false;
+		}
 	}
 
 	return ok;
@@ -97,92 +78,84 @@ free_map_write(uint32_t flags)
    sectors were available or if the free_map file could not be
    written. */
 static bool
-map_allocate(uint32_t flags, size_t cnt, block_sector_t *sectorp)
+map_allocate(struct serializable_bitmap *sb, size_t cnt, block_sector_t *out)
 {
-	const bool imap = ((flags & WRITE_INODE_MAP) != 0);
-	struct bitmap *bm = imap ? inode_map : free_map;
-
-	block_sector_t sector = bitmap_scan_and_flip(bm, 0, cnt, false);
-	if (sector != BITMAP_ERROR && !free_map_write(flags)) {
-		bitmap_set_multiple(bm, sector, cnt, false);
+	block_sector_t sector = bitmap_scan_and_flip(sb->bitmap, 0, cnt, false);
+	if (sector != BITMAP_ERROR && !map_write(sb)) {
+		bitmap_set_multiple(sb->bitmap, sector, cnt, false);
 		sector = BITMAP_ERROR;
 	}
 	if (sector != BITMAP_ERROR)
-		*sectorp = sector;
+		*out = sector;
 	return sector != BITMAP_ERROR;
 }
 
 bool
 inode_map_allocate(size_t cnt, block_sector_t *sectorp)
 {
-	return map_allocate(WRITE_INODE_MAP, cnt, sectorp);
+	return map_allocate(&inode_map, cnt, sectorp);
 }
 
 bool
 free_map_allocate(size_t cnt, block_sector_t *sectorp)
 {
-	return map_allocate(WRITE_FREE_MAP, cnt, sectorp);
+	return map_allocate(&block_map, cnt, sectorp);
 }
 
 /* Makes CNT sectors starting at SECTOR available for use. */
 static void
-map_release(uint32_t flags, block_sector_t sector, size_t cnt)
+map_release(struct serializable_bitmap *sb, block_sector_t sector, size_t cnt)
 {
-	const bool imap = ((flags & WRITE_INODE_MAP) != 0);
-	struct bitmap *bm = imap ? inode_map : free_map;
-
-	ASSERT(bitmap_all(bm, sector, cnt));
-	bitmap_set_multiple(bm, sector, cnt, false);
-
-	free_map_write(flags);
+	ASSERT(bitmap_all(sb->bitmap, sector, cnt));
+	bitmap_set_multiple(sb->bitmap, sector, cnt, false);
+	map_write(sb);
 }
 
 void
 inode_map_release(block_sector_t sector, size_t cnt)
 {
-	map_release(WRITE_INODE_MAP, sector, cnt);
+	map_release(&inode_map, sector, cnt);
 }
 
 void
 free_map_release(block_sector_t sector, size_t cnt)
 {
-	map_release(WRITE_FREE_MAP, sector, cnt);
+	map_release(&block_map, sector, cnt);
+}
+
+static void
+map_open(struct serializable_bitmap *sb)
+{
+	ASSERT(sb->sector != 0);
+	ASSERT(sb->sector_count != 0);
+
+	/* See free_map_write(). */
+	for (block_sector_t i = 0; i < sb->sector_count; ++i) {
+		void *buf = sb->memory + (i * BLOCK_SECTOR_SIZE);
+		(void)cache_read(sb->sector + i, 0, BLOCK_SECTOR_SIZE, buf);
+	}
 }
 
 /* Opens the map files and reads them from disk. */
 void
 free_map_open(void)
 {
-	ASSERT(inode_map_sector != UINT32_MAX);
-	ASSERT(inode_map_sector_count != UINT32_MAX);
-	ASSERT(free_map_sector != UINT32_MAX);
-	ASSERT(free_map_sector_count != UINT32_MAX);
-	const int sz = BLOCK_SECTOR_SIZE;
-
-	/* See free_map_write(). */
-
-	for (block_sector_t i = 0; i < inode_map_sector_count; ++i) {
-		void *buf =
-			((void *)inode_map_memory) + (i * BLOCK_SECTOR_SIZE);
-		(void)cache_read(inode_map_sector + i, 0, sz, buf);
-	}
-
-	for (block_sector_t i = 0; i < free_map_sector_count; ++i) {
-		void *buf = ((void *)free_map_memory) + (i * BLOCK_SECTOR_SIZE);
-		(void)cache_read(free_map_sector + i, 0, sz, buf);
-	}
+	map_open(&inode_map);
+	map_open(&block_map);
 }
 
 /* Writes the map files to disk. */
 void
 free_map_close(void)
 {
-	free_map_write(WRITE_INODE_MAP | WRITE_FREE_MAP);
+	map_write(&inode_map);
+	map_write(&block_map);
 }
 
 /* Creates new map files on disk and writes them. */
 void
 free_map_create(void)
 {
-	free_map_write(WRITE_INODE_MAP | WRITE_FREE_MAP);
+	map_write(&inode_map);
+	map_write(&block_map);
 }
